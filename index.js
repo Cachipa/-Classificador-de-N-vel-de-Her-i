09236ce9664e7899f6f9b153d081b0a1a11937fe
Cachipa <63456620+Cachipa@users.noreply.github.com>
2024-10-22 14:24:34 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+// Variáveis
+let nome = "Arthur";  // Nome do herói
+let experiencia = 8500;  // XP do herói
+let nivel = "";
+
+// Estrutura de decisão para classificar o nível do herói com base na XP
+if (experiencia <= 1000) {
+    nivel = "Ferro";
+} else if (experiencia >= 1001 && experiencia <= 2000) {
+    nivel = "Bronze";
+} else if (experiencia >= 2001 && experiencia <= 5000) {
+    nivel = "Prata";
+} else if (experiencia >= 5001 && experiencia <= 7000) {
+    nivel = "Ouro";
+} else if (experiencia >= 7001 && experiencia <= 8000) {
+    nivel = "Platina";
+} else if (experiencia >= 8001 && experiencia <= 9000) {
+    nivel = "Ascendente";
+} else if (experiencia >= 9001 && experiencia <= 10000) {
+    nivel = "Imortal";
+} else if (experiencia >= 10001) {
+    nivel = "Radiante";
+}
+
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
